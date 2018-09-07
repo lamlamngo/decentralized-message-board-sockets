00fc11d6729dda613e174f66b6187732d8a36fe1
Lam Ngo <ngobasonlam@gmail.com>
2018-09-07 09:34:09 -0400

    finalize main file

--- blockchain_bbs.py
+++ blockchain_bbs.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
+
 import hashlib
 import threading
 from blockchain import *
 from network import *
 from blockchain_constants import *
+import threading
 
 
 def main():
-    
+
     blockchain = Blockchain()
     blockchain_thread = threading.Thread(target=blockchain.mine)
+
     blockchain_thread.start()
-    
+
     server = Server(blockchain, True, True, True)
 
     # Main thread is server thread
